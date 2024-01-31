f64d84c59abd3537afff8186d142be9d0fdb942d
suprim-shrestha <suprim.shresth@gmail.com>
2024-01-31 16:20:27 +0545

    Add form to change timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,67 +1,81 @@
 import { useRef, useState } from "react";
+import TimerDisplay from "./TimerDisplay";
+import TimerForm from "./TimerForm";
+
+const DEFAULT_TIMER = 120;
+const TIMER_INTERVAL = 1000;
 
 function Timer() {
-  const [timer, setTimer] = useState(60);
-  const isStarted = useRef<boolean>(false);
+  const totalTime = useRef(DEFAULT_TIMER);
+  const [timer, setTimer] = useState(totalTime.current);
+  const [isStarted, setIsStarted] = useState(false);
   const timerInterval = useRef<NodeJS.Timeout>();
 
   function resetTimer() {
-    if (!isStarted.current) {
-      setTimer(60);
-    }
+    if (isStarted) return;
+
+    setTimer(totalTime.current);
   }
 
   function startTimer() {
-    if (!isStarted.current) {
-      isStarted.current = true;
-      timerInterval.current = setInterval(() => {
-        setTimer((oldTimer) => {
-          const newTimer = oldTimer - 1;
-          if (newTimer < 0) {
-            clearInterval(timerInterval.current);
-            return 0;
-          } else {
-            return newTimer;
-          }
-        });
-      }, 1000);
-    }
+    if (isStarted) return;
+
+    setIsStarted(true);
+    timerInterval.current = setInterval(() => {
+      setTimer((oldTimer) => {
+        const newTimer = oldTimer - 1;
+        if (newTimer < 0) {
+          stopTimer();
+          return 0;
+        } else {
+          return newTimer;
+        }
+      });
+    }, TIMER_INTERVAL);
   }
 
   function stopTimer() {
-    if (isStarted.current) {
-      isStarted.current = false;
-      clearInterval(timerInterval.current);
-    }
+    if (!isStarted) return;
+
+    setIsStarted(false);
+    clearInterval(timerInterval.current);
+  }
+
+  function updateTimer(newTimer: number) {
+    totalTime.current = newTimer;
+    resetTimer();
   }
 
   return (
-    <div className="timer flex items-center justify-between gap-24 py-6">
-      <h1 className="text-xl">{timer}s</h1>
-      <div className="my-3 flex flex-row gap-3">
-        <button
-          onClick={startTimer}
-          type="button"
-          className="rounded bg-gray-300 px-3 py-2 text-[#282C34] transition-all hover:bg-gray-200"
-        >
-          Start
-        </button>
-        <button
-          onClick={stopTimer}
-          type="button"
-          className="rounded bg-gray-300 px-3 py-2 text-[#282C34] transition-all hover:bg-gray-200"
-        >
-          Stop
-        </button>
-        <button
-          onClick={resetTimer}
-          type="button"
-          className="rounded bg-gray-300 px-3 py-2 text-[#282C34] transition-all hover:bg-gray-200"
-        >
-          Reset
-        </button>
+    <>
+      <TimerForm isStarted={isStarted} updateTimer={updateTimer} />
+      <div className="timer flex items-center justify-between gap-24 py-3">
+        <TimerDisplay timer={timer} />
+        <div className="my-3 flex flex-row gap-3">
+          <button
+            onClick={startTimer}
+            type="button"
+            className="rounded bg-gray-300 px-3 py-2 text-[#282C34] transition-all hover:bg-gray-200"
+          >
+            Start
+          </button>
+          <button
+            onClick={stopTimer}
+            type="button"
+            className="rounded bg-gray-300 px-3 py-2 text-[#282C34] transition-all hover:bg-gray-200"
+          >
+            Stop
+          </button>
+          <button
+            onClick={resetTimer}
+            type="button"
+            className="rounded bg-gray-300 px-3 py-2 text-[#282C34] transition-all hover:bg-gray-200"
+          >
+            Reset
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/TimerForm.tsx
@@ -0,0 +1,43 @@
+import { FormEvent, useRef } from "react";
+
+interface TimerFormProps {
+  isStarted: boolean;
+  updateTimer: (newTimer: number) => void;
+}
+
+function TimerForm(props: TimerFormProps) {
+  const timerInputRef = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (props.isStarted) return;
+
+    if (!timerInputRef.current?.value) return;
+
+    props.updateTimer(parseInt(timerInputRef.current.value));
+
+    timerInputRef.current.value = "";
+  }
+
+  return (
+    <div className="timer-input mt-10">
+      <form onSubmit={handleSubmit} className="flex">
+        <input
+          type="number"
+          className="rounded-l px-2 text-black"
+          ref={timerInputRef}
+          placeholder="Time in Seconds"
+        />
+        <button
+          type="submit"
+          className="rounded-r bg-gray-300 px-3 py-2 text-[#282C34] transition-all hover:bg-gray-200"
+        >
+          Set Timer
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default TimerForm;
