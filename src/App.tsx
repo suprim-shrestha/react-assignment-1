64a7bb077ace983072818195a61910c51015d29b
suprim-shrestha <suprim.shresth@gmail.com>
2024-02-01 15:33:06 +0545

    Add weather component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import Header from "./components/Header";
-import Timer from "./components/Timer";
+import Weather from "./components/Weather";
 
 function App() {
   return (
     <div className="text-center selection:bg-green-900">
       <div className="flex min-h-screen flex-col items-center justify-center bg-[#282c34] text-white">
-        <Header />
-        <Timer />
+        <Weather />
       </div>
     </div>
   );
