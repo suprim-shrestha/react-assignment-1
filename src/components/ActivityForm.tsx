6370a5efe53e3bb93a80d507599bcf47df012237
Suprim Shrestha <suprim.shresth@gmail.com>
2024-02-04 23:37:32 +0545

    Add form validation

--- src/components/ActivityForm.tsx
+++ src/components/ActivityForm.tsx
@@ -6,7 +6,13 @@ interface ActivityFormProps {
 }
 
 function ActivityForm(props: ActivityFormProps) {
-  const { register, handleSubmit, control } = useForm<ActivityFormData>({
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<ActivityFormData>({
+    mode: "onBlur",
     defaultValues: {
       activities: [{ description: "", timeSpent: "" }],
     },
@@ -21,7 +27,7 @@ function ActivityForm(props: ActivityFormProps) {
 
   const { setData } = props;
 
-  const onSubmit: SubmitHandler<ActivityFormData> = (data) => setData(data);
+  const onSubmitForm: SubmitHandler<ActivityFormData> = (data) => setData(data);
 
   function handleAddActivity() {
     append({ description: "", timeSpent: "" });
@@ -33,7 +39,7 @@ function ActivityForm(props: ActivityFormProps) {
 
   return (
     <form
-      onSubmit={handleSubmit(onSubmit)}
+      onSubmit={handleSubmit(onSubmitForm)}
       className="mb-10 flex flex-col gap-3 rounded-xl bg-gray-200 p-8 text-left text-black"
     >
       <div className="flex flex-col gap-2">
@@ -43,9 +49,15 @@ function ActivityForm(props: ActivityFormProps) {
         <input
           type="text"
           id="name"
-          {...register("user.name")}
+          placeholder="Name"
+          {...register("user.name", {
+            required: "Name is required",
+          })}
           className="rounded-md px-2 py-1"
         />
+        {errors.user?.name && (
+          <div className="text-red-500">{errors.user.name.message}</div>
+        )}
       </div>
       <div className="flex flex-col gap-2">
         <label className="text-lg" htmlFor="age">
@@ -55,8 +67,16 @@ function ActivityForm(props: ActivityFormProps) {
           className="rounded-md px-2 py-1"
           type="number"
           id="age"
-          {...register("user.age")}
+          placeholder="Age"
+          {...register("user.age", {
+            required: "Age is required",
+            min: { value: 0, message: "Invalid age" },
+            max: { value: 100, message: "Invalid age" },
+          })}
         />
+        {errors.user?.age && (
+          <div className="text-red-500">{errors.user.age.message}</div>
+        )}
       </div>
       <div className="flex flex-col gap-2">
         <label className="text-lg" htmlFor="contactNumber">
@@ -66,8 +86,20 @@ function ActivityForm(props: ActivityFormProps) {
           className="rounded-md px-2 py-1"
           type="text"
           id="contactNumber"
-          {...register("user.contactNumber")}
+          placeholder="Contact Number"
+          {...register("user.contactNumber", {
+            required: "Contact Number is required",
+            pattern: {
+              value: /^(?:\+\d{1,3})?\d{10}$/,
+              message: "Invalid contact number format",
+            },
+          })}
         />
+        {errors.user?.contactNumber && (
+          <div className="text-red-500">
+            {errors.user.contactNumber.message}
+          </div>
+        )}
       </div>
       {fields.map((activity, index) => {
         return (
@@ -83,22 +115,42 @@ function ActivityForm(props: ActivityFormProps) {
                 className="rounded-md px-2 py-1"
                 type="text"
                 id={`activities.${index}.description`}
-                {...register(`activities.${index}.description`)}
+                placeholder="Description"
+                {...register(`activities.${index}.description`, {
+                  required: "Description is required",
+                })}
               />
+              {errors.activities?.[index]?.description && (
+                <div className="text-red-500">
+                  {errors.activities[index]?.description?.message}
+                </div>
+              )}
             </div>
             <div className="flex flex-col gap-2">
               <label
                 className="text-lg"
-                htmlFor={`activities[${index}].timeSpent`}
+                htmlFor={`activities.${index}.timeSpent`}
               >
                 Time Spent:
               </label>
               <input
                 className="rounded-md px-2 py-1"
-                type="text"
-                id={`activities[${index}].timeSpent`}
-                {...register(`activities.${index}.timeSpent`)}
+                type="number"
+                placeholder="Time in minutes"
+                id={`activities.${index}.timeSpent`}
+                {...register(`activities.${index}.timeSpent`, {
+                  required: "Time Spent is required",
+                  pattern: {
+                    value: /^\d+$/,
+                    message: "Must be a positive integer",
+                  },
+                })}
               />
+              {errors.activities?.[index]?.timeSpent && (
+                <div className="text-red-500">
+                  {errors.activities[index]?.timeSpent?.message}
+                </div>
+              )}
             </div>
             <button
               type="button"
