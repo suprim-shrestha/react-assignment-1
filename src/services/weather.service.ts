1257f673b4192e6fd496a67abc845b54a7988dd7
suprim-shrestha <suprim.shresth@gmail.com>
2024-02-06 17:49:26 +0545

    Change to axios from fetch

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -1,6 +1,8 @@
+import axios from "axios";
+
 export async function getWeatherData() {
-  const response = await fetch(
+  const response = await axios.get(
     "https://api.open-meteo.com/v1/forecast?latitude=27.673&longitude=85.43&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,wind_speed_10m,wind_direction_10m,wind_gusts_10m&format=json&timeformat=unixtime",
   );
-  return await response.json();
+  return response.data;
 }
